9b0cbe21059d01c2a8d43c4f875ac45a9491c7d9
alotofsnakes <mrfries@gmail.com>
2022-12-13 19:13:30 -0800

    fixed database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
@@ -20,9 +20,9 @@ export const putDb = async (content) => {
   const request = store.put({ content: content });
   const result = await request;
   console.log('saved', result);
-}
+};
 
-// TODO: Add logic for a method that gets all the content from the database
+// logic for a method that gets all the content from the database
 export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
@@ -31,6 +31,6 @@ export const getDb = async () => {
   const result = await request;
   console.log('result.value', result);
   return result.value;
-}
+};
 
 initdb();
